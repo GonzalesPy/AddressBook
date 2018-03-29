b90a31092708e766a4cd3ba185da8c2159362aca
GonzalesPy <timo.zuerner@hotmail.com>
2018-03-29 13:11:14 +0100

    Changed name

--- user/userdates.py
+++ user/userdates.py
@@ -3,14 +3,16 @@ import statics
 
 
 def username(catch=None):
-    usrObj = open("username", "r")
+    usrObj = open("userdates", "r")
 
     for row in usrObj:
         statics.username = row
 
     if len(statics.username) < 2:
         statics.username = input("What is your name?: ")
-        usrObj = open("username", "w")
+        statics.myphone = input("Your phone number: ")
+        usrObj = open("userdates", "w")
         usrObj.write(statics.username)
+        usrObj.write(statics.myphone)
 
 username()
